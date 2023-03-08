41b4e858670b529de4a88097908b8b20c2111262
Klaudia Miłoszewska <klaudiamiloszewska100@gmail.com>
2023-03-08 15:10:17 +0100

    carousel update

--- src/components/imageCarousel/ImageCarousel.jsx
+++ src/components/imageCarousel/ImageCarousel.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import './imageCarousel.css';
+import {Carousel} from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import beach6 from '../../assets/beach6.jpg';
+import beach7 from '../../assets/beach7.jpg';
+import beach8 from '../../assets/beach8.jpg';
 
 function ImageCarousel() {
   return (
-    <div>
-      
-    </div>
+    <div className="container">
+    <Carousel className='carousel' showArrows={true} autoPlay infiniteLoop>
+                <div>
+                    <img src={beach6} />
+                </div>
+                <div>
+                    <img src={beach7} />
+                </div>
+                <div>
+                    <img src={beach8} />
+                </div>
+            </Carousel>
+            </div>
   )
 }
 
